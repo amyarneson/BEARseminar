bad744113c7de3e38a807605181a136a432d02b1
amyarneson <amy.arneson@berkeley.edu>
2016-10-30 20:10:59 -0700

    intial

---
+++ BEAR Seminar 2016-11-01.R
@@ -0,0 +1,15 @@
+################################################################################
+############################### INSTALLING CRASCH ##############################
+
+# This first package allows you to install from github
+install.packages("devtools")
+
+# Install crasch
+devtools::install_github("amyarneson/crasch")
+# This may take a few minutes; multiple packages will be installed
+# You may see some warning messages, and that's okay!
+
+# Use the same command to update the package as changes are made.
+
+
+################################################################################
